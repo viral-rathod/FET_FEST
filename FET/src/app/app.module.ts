339148b20dbe1619247ab9f70d68e426646d30b0
Vijay Soni <vijaysoni.net@gmail.com>
2019-06-15 12:12:59 +0530

    Minor Changes

--- FET/src/app/app.module.ts
+++ FET/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule, AppRoutedComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     SharedModule,
     AppRoutingModule,
     AppRoutingModule,
--- FET/src/app/shared/shared.module.ts
+++ FET/src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from './services/data.service';
+import { ToastrModule } from "ngx-toastr";
 import { TosterService } from './services/toster.service';
+
+const toastOption = {
+  positionClass: "toast-bottom-right"
+};
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, ToastrModule.forRoot(toastOption)],
   providers: [
     DataService,
     TosterService
