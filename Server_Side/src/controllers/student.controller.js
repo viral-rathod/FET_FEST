eb55a2fcad46d42705ac3268547cf215ef2881be
chiragma <maniarchirag57@gmail.com>
2019-06-15 16:30:01 +0530

    Student APIs upgradation

--- Server_Side/src/controllers/student.controller.js
+++ Server_Side/src/controllers/student.controller.js
@@ -34,3 +34,79 @@ exports.findAll = async (req, res) => {
     });
   }
 };
+
+exports.update = async (req, res) => {
+  try {
+    const params = req.body;
+    await StudentModel.findByIdAndUpdate(req.params.id, params);
+    res.json({
+      status: true,
+      message: "Updated successfully!"
+    });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: error,
+      data: null
+    });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    await StudentModel.findByIdAndDelete(req.params.id);
+    res.json({
+      status: true,
+      message: req.params.id + " deleted successfully!",
+      data: req.params.id
+    });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: "No record found with provided id, please try again",
+      data: null
+    });
+  }
+};
+
+exports.findById = async (req, res) => {
+  try {
+    const _data = await StudentModel.findById(req.params.id);
+    res.json({
+      status: true,
+      message: "Record fetched successfully!",
+      data: _data
+    });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: "No record found!",
+      data: null
+    });
+  }
+};
+
+exports.findByAmenities = async (req, res) => {
+  try {
+    const _query = {
+      $or: [
+        { "amenities.books.paid": false },
+        { "amenities.stationary.paid": false },
+        { "amenities.uniform.paid": false },
+        { "amenities.fee.paid": false }
+      ]
+    };
+    const _data = await StudentModel.find(_query);
+    res.json({
+      status: true,
+      message: "Record fetched successfully!",
+      data: _data
+    });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: "No record found!",
+      data: null
+    });
+  }
+};
