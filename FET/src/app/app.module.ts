608573b833eafbf444a01133266551c25dcb3713
Vijay Soni <vijaysoni.net@gmail.com>
2019-06-15 11:58:18 +0530

    Signin/ signup done

--- FET/src/app/app.module.ts
+++ FET/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule, AppRoutedComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     AppRoutingModule,
     AppRoutingModule
   ],
